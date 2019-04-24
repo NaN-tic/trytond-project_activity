e4908d108c0c8815339d7e58aae5fcb67acf2cf3
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2019-04-24 10:38:12 +0200

    Show activities even if the don't have a description.

--- work.py
+++ work.py
@@ -71,10 +71,8 @@ class Project(metaclass=PoolMeta):
     def get_conversation(self, name):
         res = []
         for activity in self.activities:
-            description_text = activity.description
+            description_text = activity.description or ''
             description_text = cgi.escape(description_text)
-            if not description_text:
-                continue
             description_text = u'<br/>'.join(description_text.splitlines())
 
             # Original Fields
