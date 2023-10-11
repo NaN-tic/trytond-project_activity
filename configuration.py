032d0b45ceb0ae948aca8bc65d682b1089e61324
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 10:36:06 +0200

    Pyflakes fixes

--- configuration.py
+++ configuration.py
@@ -1,10 +1,9 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
+from trytond.model import ModelSQL, fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
-from trytond.modules.company.model import (
-    CompanyMultiValueMixin, CompanyValueMixin)
+from trytond.modules.company.model import CompanyValueMixin
 
 
 class WorkConfiguration(metaclass=PoolMeta):
