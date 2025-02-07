f14a3a8847fa9c681cf3e913fa903255ce4c68d9
Raimon Esteve <raimonesteve@gmail.com>
2025-02-07 08:17:23 +0100

    Replace model.model to model.name

--- work.py
+++ work.py
@@ -124,7 +124,7 @@ class Project(SendActivityMailMixin, metaclass=PoolMeta):
         ProjectReference = Pool().get('project.reference')
         res = [('', '')]
         for _type in ProjectReference.search([]):
-            res.append((_type.model.model, _type.model.name))
+            res.append((_type.model.name, _type.model.string))
         return res
 
     def get_conversation(self, name):
